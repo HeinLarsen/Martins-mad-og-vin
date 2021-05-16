3a767b0f770d0726818bebb64a94e1d6cb3a7024
sygdrome <sygdrom@hotmail.com>
2021-05-16 21:03:09 +0200

    changed config

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
-import DefaultLayout from '~/layouts/Default.vue'
-import '~/assets/styles/reset.scss'
-import '~/assets/styles/global.scss'
+import DefaultLayout from '@/layouts/Default.vue'
+import '@/assets/styles/reset.scss'
+import '@/assets/styles/global.scss'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
