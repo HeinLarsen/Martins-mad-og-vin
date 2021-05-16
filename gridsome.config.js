3732486bc7b832b9d2ec1d8ad755d05434b63873
sygdrome <sygdrom@hotmail.com>
2021-05-16 21:08:59 +0200

    changes

--- gridsome.config.js
+++ gridsome.config.js
@@ -6,7 +6,7 @@ module.exports = {
   siteName: config.title,
 
   siteUrl: 'https://www.martinsmadogvin.dk',
-  pathPrefix: '',
+  // pathPrefix: '/Martins-mad-og-vin',
   //make editable config & data files available to graphql
   metadata: {
     siteName: config.title,
